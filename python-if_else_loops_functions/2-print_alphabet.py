b884cb1dbd37190f316e76913db0fa66b13bb0c8
gastonperego <gastonperego@gmail.com>
2023-09-19 14:52:24 +0000

    feat: all the excercises added

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+i = ord('a')
+for i in range(97, 123):
+    print(chr(i), end = "")
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+i = ord('a')
+for i in range(97, 123):
+    if chr(i) == 'e' or chr(i) == 'q':
+        i += 2
+    else:
+        print(chr(i), end = "")
