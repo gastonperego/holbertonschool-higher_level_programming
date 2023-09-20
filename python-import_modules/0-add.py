059b1178f51d4059598615728c7973433a4bf775
gastonperego <gastonperego@gmail.com>
2023-09-20 21:45:21 +0000

    fix: string format

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-import add_0
+
+from add_0 import add
 
 a = 1
 b = 2
 
-print(f"{add_0.add(a, b)}")
+print("{}".format(add(a, b)))
--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,12 +3,12 @@
 from sys import *
 
 if len(argv) == 1:
-    print(f"0 arguments.")
+    print("0 arguments.")
 elif len(argv) == 2:
-    print(f"1 argument:\n1: {argv[1]}")
+    print("1 argument:\n1: {}".format(argv[1]))
 else:
-    print(f"{len(argv) - 1} arguments:")
+    print("{} arguments:".format(len(argv) - 1))
     i = 1
     while i < len(argv):
-        print(f"{i}: {argv[i]}")
+        print("{}: {}".format(i, argv[i]))
         i += 1
