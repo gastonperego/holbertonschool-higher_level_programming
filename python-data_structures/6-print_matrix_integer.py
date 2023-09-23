d3d528eeb3fe5aa8d917b35574af8946f1123112
gastonperego <gastonperego@gmail.com>
2023-09-23 16:04:09 +0000

    fix: minor bugs

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             print("{:d} ".format(matrix[i][j]), end = "")
-        print()
+        print("")
