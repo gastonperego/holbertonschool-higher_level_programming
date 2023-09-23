cce54c545c45452d4bfdbbe3d107c45a159768a2
gastonperego <gastonperego@gmail.com>
2023-09-23 15:05:41 +0000

    fix: minor bugs

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,6 +5,6 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return None
     i = 0
-    while i < len(my_list) - 1:
+    while i < len(my_list):
         i += 1
     return i
