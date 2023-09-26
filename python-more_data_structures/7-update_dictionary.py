19da5fdedbce3c41cba78d82fb1a646d5a42a989
gastonperego <gastonperego@gmail.com>
2023-09-26 14:20:22 +0000

    fix: Less code

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary:
-        if i == key:
-            a_dictionary[key] = value
-    if key not in a_dictionary:
-        a_dictionary.update({key: value})
+    a_dictionary.update({key: value})
     return a_dictionary
