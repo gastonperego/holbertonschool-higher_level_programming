5e8484b996ccea2db03d86a9543f3c6c77a9bc6c
gastonperego <gastonperego@gmail.com>
2023-09-26 14:15:36 +0000

    feat: update dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    for i in a_dictionary:
+        if i == key:
+            a_dictionary[key] = value
+    if key not in a_dictionary:
+        a_dictionary.update({key: value})
+    return a_dictionary
