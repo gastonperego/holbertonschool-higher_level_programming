0d2d06ceaabce5462b0eb05f673e1fcf923e93e1
gastonperego <gastonperego@gmail.com>
2023-09-27 15:05:23 +0000

    fix: pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    if not my_list or my_list == [] or my_list is None:
+        return 0
+    
     try:
         for i in range(0, x):
             print(my_list[i], end="")
