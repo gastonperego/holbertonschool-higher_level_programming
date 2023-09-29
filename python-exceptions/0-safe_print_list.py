8bf7f7b7c4eceb9f9ede8684dd3bc769bfe48989
gastonperego <gastonperego@gmail.com>
2023-09-29 15:21:55 +0000

    fix: empty list case

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    if not my_list or my_list == [] or my_list is None:
+    if my_list is None:
         return 0
     a = 0
     for i in range(x):
