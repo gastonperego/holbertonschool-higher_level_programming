4d29f405a3bf635b292d93e69153d6e4f9792efd
gastonperego <gastonperego@gmail.com>
2023-09-30 22:33:43 +0000

    fix: raise errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,10 +8,11 @@ class Square:
 
     def __init__(self, size=0):
         if isinstance(size, int):
-            if size > 0:
+            if size >= 0:
                 self.__size = size
             else:
-                print("size must be >= 0")
+                raise ValueError("size must be >= 0")
         else:
-            print("size must be an integer")
+            raise TypeError("size must be an integer")
+        
             
