86bad85c6407bd6b17c1f3ab99f4f29f74a13b14
gastonperego <gastonperego@gmail.com>
2023-10-01 14:40:12 +0000

    feat: square 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Square  """
+
+
+class Square:
+    """Square with a given size """
+    __size = None
+
+    
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+    
+
+        
+    
+    
+    
