75650ebd7b23a8c05867f210a506be06719a64e4
gastonperego <gastonperego@gmail.com>
2023-10-02 13:26:18 +0000

    fix: minor bugs

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,18 @@
 class Rectangle:
     """ Rectangle: empty class"""
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        elif height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
 
     @property
     def height(self):
