cc6152e948cb4b18fd21cf415e13b3b0253d4022
gastonperego <gastonperego@gmail.com>
2023-10-02 13:36:14 +0000

    feat: rectangle 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Rectangle: empty class"""
+
+
+class Rectangle:
+    """ Rectangle: empty class"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return (self.width * 2) + (self.height * 2)
