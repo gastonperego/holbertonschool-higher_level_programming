8982eec28701eea3d743fab3461d0ad5064389f2
root <gastonperego@gmail.com>
2023-10-03 02:01:30 +0000

    fix: TypeError

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -8,8 +8,18 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
         Rectangle.number_of_instances += 1
 
     print_symbol = "#"
@@ -82,8 +92,4 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        if not isinstance(size, int):
-            raise TypeError("width must be an integer")
-        if size < 0:
-            raise ValueError("width must be >= 0")
         return cls(size, size)
