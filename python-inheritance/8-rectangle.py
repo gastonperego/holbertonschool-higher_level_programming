a48adf57a309fa480bfbd35fdf120441259d79c4
gastonperego <gastonperego@gmail.com>
2023-10-05 23:36:43 +0000

    fix: case name is not a str

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,8 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """a"""
-        if type(name) is not str:
-            raise TypeError("name must be a string")
+        
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         elif value < 1:
