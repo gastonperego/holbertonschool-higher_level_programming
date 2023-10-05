df111eadd8f95d938e59656b748f7785bf2d326e
gastonperego <gastonperego@gmail.com>
2023-10-05 22:56:58 +0000

    fix: documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,9 +6,11 @@ class BaseGeometry:
     """a"""
 
     def area(self):
+        """a"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """a"""
         if type(name) is not str:
             raise TypeError("name must be a string")
         if type(value) is not int:
