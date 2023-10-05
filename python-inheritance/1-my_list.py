b93fea189390798675ba3d5e6bd1935e34581968
gastonperego <gastonperego@gmail.com>
2023-10-05 14:29:43 +0000

    fix: requirements

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """A"""
 
+
 class MyList(list):
+    """" d """
 
     def print_sorted(self):
-        print(sorted(self))
+        """" d """
+
+        if all(isinstance(i, int) for i in self):
+            print(sorted(self))
+        else:
+            raise TypeError("All elements must be integers")
