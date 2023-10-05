1b3430596f967486c1f8add6157fd8cd4c55958d
gastonperego <gastonperego@gmail.com>
2023-10-05 23:29:25 +0000

    feat: base_geometry and tests

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""s"""
+
+
+class BaseGeometry:
+    """a"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(name) is not str:
+            raise TypeError("name must be a string")
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value < 1:
+            raise ValueError(f"{name} must be greater than 0")
