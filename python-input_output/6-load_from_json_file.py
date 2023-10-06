d7e18af8fea3395e7382c72be4cf55144e1c359d
gastonperego <gastonperego@gmail.com>
2023-10-06 21:16:09 +0000

    feat: load json file2

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""create an object from a json file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """create an object from a json file"""
+
+    with open(filename, "r", encoding="utf-8") as file:
+        obj = file.read()
