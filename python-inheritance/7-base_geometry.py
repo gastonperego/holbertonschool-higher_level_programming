8475c83695955a2f1ba1aa18ddb79505f93c6109
gastonperego <gastonperego@gmail.com>
2023-10-06 12:53:04 +0000

    fix: doc: tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,8 +19,8 @@ class BaseGeometry:
         """
         Function that validates an integer
         """
-        if isinstance(name, str):
-            if not isinstance(value, int):
-                raise TypeError(f"{name} must be an integer")
-            elif value < 1:
-                raise ValueError(f"{name} must be greater than 0")
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value < 1:
+            raise ValueError(f"{name} must be greater than 0")
