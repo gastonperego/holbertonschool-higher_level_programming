0f05579f9f177aa4d85b4ebe14fbc57d11dd6e13
gastonperego <gastonperego@gmail.com>
2023-10-06 12:33:09 +0000

    fix: documentation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""Checker la recalcada concha de tu madre"""
+"""A"""
 
 
 class BaseGeometry:
-    """Checker la recalcada concha de tu madre"""
+    """Class BaseGeometry"""
 
     def area(self):
-        """Checker la recalcada concha de tu madre"""
+        """Function area not available yet"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Checker la recalcada concha de tu madre"""
-        if type(name) is not str:
-            raise TypeError("name must be a string")
+        """Function that validates an integer"""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         elif value < 1:
