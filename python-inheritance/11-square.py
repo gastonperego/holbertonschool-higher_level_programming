b5244c0699822a573f8b89ece29fb74fc3d6fce5
gastonperego <gastonperego@gmail.com>
2023-10-06 13:38:26 +0000

    feat: square 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class square"""
+
+    def __init__(self, size):
+        """init"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self.size}/{self.size}"
