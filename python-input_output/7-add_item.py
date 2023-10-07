50f655499830c0cbb20d5cb28db99e7236be1b07
gastonperego <gastonperego@gmail.com>
2023-10-07 19:18:06 +0000

    fix: name of the file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,12 +7,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 try:
-    my_list = load_from_json_file("add_item.py")
+    my_list = load_from_json_file("add_item.json")
 
-except (FileNotFoundError):
+except:
     my_list = []
 finally:
-    with open("add_item.py", "w", encoding="utf-8"):
+    with open("add_item.json", "w", encoding="utf-8"):
         for i in range(1, len(argv)):
             my_list.append(argv[i])
-        save_to_json_file(my_list, "add_item.py")
+        save_to_json_file(my_list, "add_item.json")
