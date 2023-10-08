16ed1fd917bd28e0914700fac79957e2964af20e
gastonperego <gastonperego@gmail.com>
2023-10-08 14:35:45 +0000

    fix: return

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python
-"""class stu"""
+#!/usr/bin/python3
+"""
+    Start of the program
+"""
 
 
 class Student():
-    """class student"""
-
-    def __init__(self, first_name, last_name, age):
-        
-
+    """
+        Creation of the class student
+    """
+    def _init_(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return self.__dict__
+        return self._dict_
