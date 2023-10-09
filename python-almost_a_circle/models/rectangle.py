5634d26a7e85bbbc5088cdeb0e7cd574afb49fa9
gastonperego <gastonperego@gmail.com>
2023-10-09 14:09:13 +0000

    fix: documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,40 +19,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter"""
+
         return self.__width
 
     width.setter
 
     def width(self, value):
+        """setter"""
+
         if type(value) is int and value >= 0:
             self.__width = value
 
     @property
     def height(self):
+        """getter"""
+
         return self.__height
 
     height.setter
 
     def height(self, value):
+        """setter"""
+
         if type(value) is int and value >= 0:
             self.__height = value
 
     @property
     def x(self):
+        """getter"""
+
         return self.__x
 
     x.setter
 
     def x(self, value):
+        """setter"""
+
         if type(value) is int and value >= 0:
             self.__x = value
 
     @property
     def y(self):
+        """getter"""
+
         return self.__y
 
     y.setter
 
     def y(self, value):
+        """setter"""
+
         if type(value) is int and value >= 0:
             self.__y = value
