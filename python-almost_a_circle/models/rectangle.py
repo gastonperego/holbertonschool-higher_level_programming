deecaf327aa946acfb3cd6d2077a6c78834bd13e
gastonperego <gastonperego@gmail.com>
2023-10-09 14:01:56 +0000

    fix: pyco

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,13 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """Rectangle class"""
 
-    def __init__(self, width=0, height=0, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init"""
+
         id = super().__init__(id)
         self.__width = width
         self.__height = height
@@ -17,8 +20,9 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+
     width.setter
+
     def width(self, value):
         if type(value) is int and value >= 0:
             self.__width = value
@@ -26,8 +30,9 @@ class Rectangle(Base):
     @property
     def height(self):
         return self.__height
-    
+
     height.setter
+
     def height(self, value):
         if type(value) is int and value >= 0:
             self.__height = value
@@ -35,8 +40,9 @@ class Rectangle(Base):
     @property
     def x(self):
         return self.__x
-    
+
     x.setter
+
     def x(self, value):
         if type(value) is int and value >= 0:
             self.__x = value
@@ -44,8 +50,9 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-    
+
     y.setter
+
     def y(self, value):
         if type(value) is int and value >= 0:
             self.__y = value
