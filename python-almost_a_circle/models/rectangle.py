3dcf53176cbd23dc272e9d134ee533f311af493c
gastonperego <gastonperego@gmail.com>
2023-10-10 14:52:23 +0000

    feat: rectangle 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,17 @@ class Rectangle(Base):
 
         string1 = f"{self.__width}/{self.__height}"
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " + string1)
+
+    def update(self, *args):
+        """updates the values of th rectangle"""
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
