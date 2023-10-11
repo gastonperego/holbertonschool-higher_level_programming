bc938bd5c080672988ba44982d5d46603d9e382a
gastonperego <gastonperego@gmail.com>
2023-10-11 13:31:00 +0000

    feat: square 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,4 +7,25 @@ class Square(Rectangle):
     """Class square"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """init"""
+        
         super().__init__(size, size, x, y, id)
+        self.__size = size
+        
+    @property
+    def size(self):
+        """size"""
+        
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__size = value
+        
