664c494c1f878b54d2652a7e2888448c5167bca3
gastonperego <gastonperego@gmail.com>
2023-10-12 23:49:55 +0000

    fix: documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,7 +40,7 @@ class Base:
     def from_json_string(json_string):
         """converts a json string into a list"""
 
-        if json_string is None or len(json_string) == 0:
+        if json_string is None or len(json_string) == 0 or json_string == "":
             return ""
         else:
             return json.loads(json_string)
