dcb14235bbf5906930eb389eac2e111da4164bdc
gastonperego <gastonperego@gmail.com>
2023-10-13 23:32:46 +0000

    added:  more tests

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -13,5 +13,40 @@ class RectangleTests(unittest.TestCase):
     def test_no_args(self):
         with self.assertRaises(TypeError):
             r1 = Rectangle()
-        
+    
+    def test_print_private_attribute_1(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(AttributeError):
+            print(r1.__width)
+
+    def test_print_private_attribute_2(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(AttributeError):
+            print(r1.__height)
+
+    def test_print_private_attribute_3(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(AttributeError):
+            print(r1.__x)
+
+    def test_print_private_attribute_4(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(AttributeError):
+            print(r1.__y)
+
+    def test_getter_works_1(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        self.assertEqual(2, r1.width)
+
+    def test_getter_works_1(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        self.assertEqual(2, r1.height)
+
+    def test_getter_works_1(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        self.assertEqual(2, r1.x)
+
+    def test_getter_works_1(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        self.assertEqual(2, r1.y)
    
