dd0382d6a890abc508fb5283eea2af99f73f95e7
gastonperego <gastonperego@gmail.com>
2023-11-11 20:37:17 +0000

    feat: task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creates a second table
+-- creates a second table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT second_table (id, name, score) VALUES (1, 'Jhon', 10);
+INSERT second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT second_table (id, name, score) VALUES (4, 'George', 8);
