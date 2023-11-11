79fd0b1fd03a2882ea65f84829b5247a070df1e0
gastonperego <gastonperego@gmail.com>
2023-11-11 21:06:22 +0000

    fix: only one insertion

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,4 @@
 -- creates a second table
 -- creates a second table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT second_table (id, name, score) VALUES (1, "Jhon", 10);
-INSERT second_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT second_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT second_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "Jhon", 10) (2, "Alex", 3) (3, "Bob", 14) (4, "George", 8);
