309986c2f6d93fcedc16625b6fc639a69012cda7
gastonperego <gastonperego@gmail.com>
2023-11-14 13:08:51 +0000

    feat: task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- creates the database hbtn_0d_usa and the table states
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
