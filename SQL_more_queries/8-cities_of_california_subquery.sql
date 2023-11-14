9bb312eea9c0b3716f7094323dd9ae5b7921fd10
gastonperego <gastonperego@gmail.com>
2023-11-14 13:33:27 +0000

    feat: task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT * FROM states WHERE name='California' ORDER BY cities.id ASC;
