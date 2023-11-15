29e5939822c0adaa8c3e5fd537831b214dd2c780
gastonperego <gastonperego@gmail.com>
2023-11-15 14:00:25 +0000

    feat: task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,3 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT title, tv_show_genres.show_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL;
+SELECT title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL;
