658e64e41da3e0e701c35bb9304fcf5ea18c80a4
gastoneprego <gastonperego@gmail.com>
2023-11-18 17:10:00 +0000

    ix: minor bugs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost", user=argv[1], passswd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.excecute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cur:
         print(row)
     cur.close()
