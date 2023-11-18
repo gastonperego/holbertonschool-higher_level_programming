a1ea1843c10040df02006911e2eb03fbd89cc104
gastoneprego <gastonperego@gmail.com>
2023-11-18 17:04:10 +0000

    feat: task 0 and README

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=argv[1], passswd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.excecute("SELECT * FROM states ORDER BY states.id ASC")
+    table = cur.fetchall()
+    for row in table:
+        print(row)
