5b5d178d6e0f5e2eddcd4afc619c74337bb202f4
gastonperego <gastonperego@gmail.com>
2023-11-18 21:29:45 +0000

    fix: pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    lists all states with a name starting with N (upper N) from the database 
+    lists all states with a name starting with N (upper N) from the database
     hbtn_0e_0_usa
 """
 
