26211bbce8c74bc2a5be1593a12849699544659e
gastoneprego <gastonperego@gmail.com>
2023-11-18 17:13:22 +0000

    fix: minor bugs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
-
-
-import MySQLdb
 from sys import argv
+import MySQLdb
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], passswd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cur:
