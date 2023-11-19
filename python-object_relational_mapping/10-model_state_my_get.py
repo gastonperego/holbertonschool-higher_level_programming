4383ff6e4c052b7be8d12a69c21ec7edfedcae29
gastonperego <gastonperego@gmail.com>
2023-11-19 21:17:46 +0000

    feat: task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    prints the State object with the name passed as argument
+     from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    engine = create_engine(f"mysql://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+    Session = sessionmaker(engine)
+    session = Session()
+    rows = session.query(State).filter(name=argv[4]).order_by(State.id)
+    if rows:
+        print(rows.id)
+    else:
+        print("Not found")
