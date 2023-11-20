a3e8ec7ab17f5cc45813081ee93b4012c656e202
gastonperego <gastonperego@gmail.com>
2023-11-20 02:13:33 +0000

    fix: minor bigs

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """
-  write a script 14-model_city_fetch_by_state.py that
-  prints all City objects from the database hbtn_0e_14_usa
-
+    deletes all State objects with a name containing the letter
+      a from the database hbtn_0e_6_usa
 """
-
-import sys
+from sys import argv
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-      Main function.
-    """
-    conn = 'mysql+mysqldb://{}:{}@localhost/{}'
-    engine = create_engine(conn.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    engine = create_engine(f"mysql://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    res = session.query(City, State)
-    res = res.join(State, State.id == City.state_id).all()
-
-    for city, state in res:
-        print(f"{state.name}: ({city.id}) {city.name}")
-
-    session.close()
+    table = session.query(State).join(City)
+    for row in table:
+        print(f"{row.State.name}: ({row.City.id}) {row.City.name}")
