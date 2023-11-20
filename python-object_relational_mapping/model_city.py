136bac369aaa7cdf30660a8e20a8966d98326943
gastonperego <gastonperego@gmail.com>
2023-11-20 02:13:01 +0000

    fix: pyco

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -16,4 +16,5 @@ class City(Base):
     id = Column("id", Integer, unique=True, autoincrement=True,
                 primary_key=True, nullable=False)
     name = Column("name", String(128), nullable=False)
-    state_id = Column("state_id", Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column("state_id", Integer, ForeignKey('states.id'),
+                      nullable=False)
