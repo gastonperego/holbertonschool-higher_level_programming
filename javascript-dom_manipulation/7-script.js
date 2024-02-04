ce619ab71cd770efb15d7994a692a522c2dc9706
gastonperego <gastonperego@gmail.com>
2024-02-04 21:03:03 +0000

    feat: task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+
+.then((res) => {
+    return res.json()
+})
+
+.then((data) => {
+    const list = document.querySelector('#list_movies')
+    for (movie of data.results) {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(movie.title))
+        list.appendChild(li)
+    }})
