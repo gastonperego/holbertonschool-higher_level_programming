d36a9819a2dd78cc17fd57506b03c64d05a82107
gastonperego <gastonperego@gmail.com>
2024-02-04 21:16:38 +0000

    feat: task 8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+
+.then((res) => {
+    return res.json()
+})
+
+.then((data) => {
+    const element = document.getElementById('hello')
+    element.innerText = data.hello
+})
