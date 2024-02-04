effd782e95bb0e608c1050e6dd874b91f2efe48f
gastonperego <gastonperego@gmail.com>
2024-02-04 16:41:09 +0000

    feat: task 3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById('toggle_header')
+toggle.onclick = function () {
+    const header = document.querySelector('header')
+    console.log(header.classList)
+    if (header.classList.contains('green')) {
+        header.classList.replace('green', 'red')
+    } else if (header.classList.contains('red')) {
+        header.classList.replace('red', 'green')
+    }
+}
